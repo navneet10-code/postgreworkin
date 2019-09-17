2cc9baf1fa1b0dc63bbf9f4c2817d9865001d4cf
navneet10-code <54025358+navneet10-code@users.noreply.github.com>
2019-09-17 10:20:41 +0530

    Update web.js

--- web.js
+++ web.js
@@ -54,3 +54,19 @@ app.listen(port, function() {
 console.log("Listening on " + port);
 });
 
+
+
+//var express = require('express')
+var bodyParser = require('body-parser')
+ 
+//var app = express()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+
+app.post('/login', urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.send('welcome, ' + req.body.username)
+})
+
+
